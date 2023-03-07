afba4934820e314e6f80430a083ad402ecc93d6e
Ashley@Pikselin <ash@pikselin.com>
2023-03-07 13:32:28 +1300

    update security policy

--- src/extensions/SecurityPolicy.php
+++ src/extensions/SecurityPolicy.php
@@ -12,18 +12,32 @@ use SilverStripe\ORM\DataExtension;
          * @config
          */
         private static $headers = [];
+        private static $csp_headers = [];
         
         public function onBeforeInit() {
-            
-            // add csp headers
-            
-            
+            print_r(self::$headers);
+            print_r(self::$csp_headers);
+            // base headers
+            if(is_array(self::$headers)) {
             foreach (self::$headers as $k => $v) {
-                $this->owner->getResponse()->addHeader($k, $v);    
+                $this->owner->getResponse()->addHeader($k, $v);
+            }                
             }
-            
-            
 
+            
+            // CSP headers
+            if(is_array(self::$csp_headers)) {
+                $directives = [];
+                foreach(self::$csp_headers as $k => $v) {
+                    // is the value also an array
+                    if(is_array($v)) {
+                        $vals = implode(' ',$v);
+                        $directives[] = $k.' '.$vals.';';
+                    }
+                }
+                $cspdirectives = implode('', $directives);
+                $this->owner->getResponse()->addHeader('Content-Security-Policy', $cspdirectives);
+            }
         }
     }
 }
