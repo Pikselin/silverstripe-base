53bcca13120ee3bd2511cac054e15b2a412d8284
Ashley@Pikselin <ash@pikselin.com>
2023-03-09 12:02:49 +1300

    disable everything

--- src/extensions/BaseContentController.php
+++ src/extensions/BaseContentController.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace Pikselin\base {
-
-    use SilverStripe\ORM\DataExtension;
-    use SilverStripe\SiteConfig\SiteConfig;
-    use SilverStripe\View\SSViewer;
-
-    class BaseContentController extends DataExtension {
-
-        public function onBeforeInit() {
-            
-            $SiteConfig = SiteConfig::current_site_config();
-            if (!empty($SiteConfig->OverrideTheme)) {
-                SSViewer::set_themes([$SiteConfig->OverrideTheme, SSViewer::DEFAULT_THEME]);
-            }
-        }
-    }
-}
+//namespace Pikselin\base {
+//
+//    use SilverStripe\ORM\DataExtension;
+//    use SilverStripe\SiteConfig\SiteConfig;
+//    use SilverStripe\View\SSViewer;
+//
+//    class BaseContentController extends DataExtension {
+//
+//        public function onBeforeInit() {
+//            
+//            $SiteConfig = SiteConfig::current_site_config();
+//            if (!empty($SiteConfig->OverrideTheme)) {
+//                SSViewer::set_themes([$SiteConfig->OverrideTheme, SSViewer::DEFAULT_THEME]);
+//            }
+//        }
+//    }
+//}
