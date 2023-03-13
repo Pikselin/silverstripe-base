1aa1648f256a0ad1a0c2a98ba1fd38feaccf4014
Ashley@Pikselin <ash@pikselin.com>
2023-03-14 12:53:50 +1300

    add comments to empty Page extension class

--- src/extensions/SVGIconPageExtension.php
+++ src/extensions/SVGIconPageExtension.php
@@ -2,11 +2,13 @@
 
 namespace Pikselin\base {
 
-use SilverStripe\ORM\DataExtension;
+    use SilverStripe\ORM\DataExtension;
 
     class SVGIconPageExtension extends DataExtension {
-
-
+        
+        /**
+         * Add $db field extension to pull in an id selector, might be best handled directly by the bespoke /app/Page Types method though to keep it agnostic
+         */
+    }
 
 }
-
