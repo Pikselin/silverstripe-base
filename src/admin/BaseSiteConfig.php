642ab9efc50d210aab31955d50fc898a20fea838
Ashley@Pikselin <ash@pikselin.com>
2023-08-07 15:01:44 +1200

    New fields added

--- src/admin/BaseSiteConfig.php
+++ src/admin/BaseSiteConfig.php
@@ -16,15 +16,19 @@ namespace Pikselin\base {
             'TagManager'    => 'Text',
             'GACode'        => 'Text',
             'PB_OverrideTheme' => 'Varchar(255)',
-//            'GoogleMapsAPIKey' => 'Text',
-//            'YouTubeAPIKey' => 'Text',
+            'GoogleMapsAPIKey' => 'Varchar(255)',
+            'YouTubeAPIKey' => 'Varchar(255)',
         ];
 
         public function updateCMSFields(FieldList $fields)
         {
             $fields->addFieldToTab('Root.Main', EmailField::create('SiteEmail', 'General Email address'));
-            $fields->addFieldToTab('Root.3rdPartyTools', TextField::create('TagManager', 'Google Tag Manager key'));
-            $fields->addFieldToTab('Root.3rdPartyTools', TextField::create('GACode', 'Google Analytics key'));
+            $APIKeysLead = LiteralField::create('APIKeysLead','<p>Some features will require API keys in order to access functions such as website tracking or video embedding. Enter API keys below for the features required by this site.</p>');
+            $fields->addFieldToTab('Root.3rdPartyTools', $APIKeysLead);
+            $fields->addFieldToTab('Root.3rdPartyTools', TextField::create('TagManager', 'Google Tag Manager key')->setDescription('<a href="https://support.google.com/tagmanager/answer/6103696?hl=en" target="_blank">Set up and install Google Tag Manager</a>'));
+            $fields->addFieldToTab('Root.3rdPartyTools', TextField::create('GACode', 'Google Analytics key')->setDescription('<a href="https://support.google.com/analytics/answer/9304153?hl=en" target="_blank">Set up and install Google Analytics</a>'));
+            $fields->addFieldToTab('Root.3rdPartyTools', TextField::create('GoogleMapsAPIKey', 'Google Maps API key')->setDescription('<a href="https://developers.google.com/maps/documentation/embed/get-api-key" target="_blank">Set up and install Google Maps API key</a>'));
+            $fields->addFieldToTab('Root.3rdPartyTools', TextField::create('YouTubeAPIKey', 'YouTube API key')->setDescription('<a href="https://developers.google.com/youtube/v3/getting-started" target="_blank">Get a YouTube API key</a>'));
 
 
 
